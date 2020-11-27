a34a9bb18d500b903a93ecdbf3f62d568b9e4d81
Andrenoj11 <andrenojuanda94@gmail.com>
2020-11-27 12:42:46 +0700

    resolve conflict in helper.js and main.js

--- assets/main.js
+++ assets/main.js
@@ -2,11 +2,6 @@ $(document).ready(function(){
     if (localStorage.getItem('access_token')) {
         showMainPage()
         fetchIgdbAPI()
-<<<<<<< HEAD
-=======
-        // fetchJokesAPI()
-        // fetchTriviaAPI()
->>>>>>> 4e0a55ee0edf149954913e59efd649fc4c2ca875
     } else {
         showLoginPage()
     }
@@ -19,7 +14,6 @@ $(document).ready(function(){
     $("#logout-button").on("click", function () {
         logout()
     });
-<<<<<<< HEAD
 
     $("#trivia-btn").on("click", function () {
         fetchTrivia()
@@ -33,6 +27,3 @@ $(document).ready(function(){
         jokes()
     });
 }); 
-=======
-}); 
->>>>>>> 4e0a55ee0edf149954913e59efd649fc4c2ca875
